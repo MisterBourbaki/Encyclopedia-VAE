30a1a76b918cce78a7bd4562b177fd2f5cfd5af1
BaptisteMorisse <bmorisse.math@protonmail.com>
2024-04-30 10:34:34 +0200

    Refacto: Conditionnal VAE model.

--- encyclopedia_vae/models/cvae.py
+++ encyclopedia_vae/models/cvae.py
@@ -1,132 +1,33 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
 
-from encyclopedia_vae.models import BaseVAE
+from encyclopedia_vae.models.vanilla_vae import VanillaVAE
+from encyclopedia_vae.types_helpers import ForwardReturn
 
 
-class ConditionalVAE(BaseVAE):
+class ConditionalVAE(VanillaVAE):
     def __init__(
         self,
         in_channels: int,
         num_classes: int,
         latent_dim: int,
-        hidden_dims: list = None,
+        hidden_dims: list = [32, 64, 128, 256, 512],
         img_size: int = 64,
-        **kwargs,
     ) -> None:
-        super().__init__()
+        super().__init__(
+            in_channels=in_channels + 1,
+            latent_dim=latent_dim,
+            hidden_dims=hidden_dims,
+            latent_dim_dec=latent_dim + num_classes,
+        )
 
-        self.latent_dim = latent_dim
         self.img_size = img_size
 
         self.embed_class = nn.Linear(num_classes, img_size * img_size)
         self.embed_data = nn.Conv2d(in_channels, in_channels, kernel_size=1)
 
-        modules = []
-        if hidden_dims is None:
-            hidden_dims = [32, 64, 128, 256, 512]
-
-        in_channels += 1  # To account for the extra label channel
-        # Build Encoder
-        for h_dim in hidden_dims:
-            modules.append(
-                nn.Sequential(
-                    nn.Conv2d(
-                        in_channels,
-                        out_channels=h_dim,
-                        kernel_size=3,
-                        stride=2,
-                        padding=1,
-                    ),
-                    nn.BatchNorm2d(h_dim),
-                    nn.LeakyReLU(),
-                )
-            )
-            in_channels = h_dim
-
-        self.encoder = nn.Sequential(*modules)
-        self.fc_mu = nn.Linear(hidden_dims[-1] * 4, latent_dim)
-        self.fc_var = nn.Linear(hidden_dims[-1] * 4, latent_dim)
-
-        # Build Decoder
-        modules = []
-
-        self.decoder_input = nn.Linear(latent_dim + num_classes, hidden_dims[-1] * 4)
-
-        hidden_dims.reverse()
-
-        for i in range(len(hidden_dims) - 1):
-            modules.append(
-                nn.Sequential(
-                    nn.ConvTranspose2d(
-                        hidden_dims[i],
-                        hidden_dims[i + 1],
-                        kernel_size=3,
-                        stride=2,
-                        padding=1,
-                        output_padding=1,
-                    ),
-                    nn.BatchNorm2d(hidden_dims[i + 1]),
-                    nn.LeakyReLU(),
-                )
-            )
-
-        self.decoder = nn.Sequential(*modules)
-
-        self.final_layer = nn.Sequential(
-            nn.ConvTranspose2d(
-                hidden_dims[-1],
-                hidden_dims[-1],
-                kernel_size=3,
-                stride=2,
-                padding=1,
-                output_padding=1,
-            ),
-            nn.BatchNorm2d(hidden_dims[-1]),
-            nn.LeakyReLU(),
-            nn.Conv2d(hidden_dims[-1], out_channels=3, kernel_size=3, padding=1),
-            nn.Tanh(),
-        )
-
-    def encode(self, input: torch.tensor) -> list[torch.tensor]:
-        """
-        Encodes the input by passing through the encoder network
-        and returns the latent codes.
-        :param input: (torch.tensor) Input tensor to encoder [N x C x H x W]
-        :return: (torch.tensor) List of latent codes
-        """
-        result = self.encoder(input)
-        result = torch.flatten(result, start_dim=1)
-
-        # Split the result into mu and var components
-        # of the latent Gaussian distribution
-        mu = self.fc_mu(result)
-        log_var = self.fc_var(result)
-
-        return [mu, log_var]
-
-    def decode(self, z: torch.tensor) -> torch.tensor:
-        result = self.decoder_input(z)
-        result = result.view(-1, 512, 2, 2)
-        result = self.decoder(result)
-        result = self.final_layer(result)
-        return result
-
-    def reparameterize(self, mu: torch.tensor, logvar: torch.tensor) -> torch.tensor:
-        """
-        Will a single z be enough ti compute the expectation
-        for the loss??
-        :param mu: (torch.tensor) Mean of the latent Gaussian
-        :param logvar: (torch.tensor) Standard deviation of the latent Gaussian
-        :return:
-        """
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return eps * std + mu
-
-    def forward(self, input: torch.tensor, **kwargs) -> list[torch.tensor]:
-        y = kwargs["labels"].float()
+    def forward(self, input: torch.tensor, labels: torch.tensor) -> ForwardReturn:
+        y = labels.float()
         embedded_class = self.embed_class(y)
         embedded_class = embedded_class.view(
             -1, self.img_size, self.img_size
@@ -134,30 +35,16 @@ class ConditionalVAE(BaseVAE):
         embedded_input = self.embed_data(input)
 
         x = torch.cat([embedded_input, embedded_class], dim=1)
-        mu, log_var = self.encode(x)
-
-        z = self.reparameterize(mu, log_var)
+        latents = self.encode(x)
+        mu, log_var, _ = latents.values()
+        z = self.reparametrize(mu, log_var)
 
         z = torch.cat([z, y], dim=1)
-        return [self.decode(z), input, mu, log_var]
-
-    def loss_function(self, *args, **kwargs) -> dict:
-        recons = args[0]
-        input = args[1]
-        mu = args[2]
-        log_var = args[3]
-
-        kld_weight = kwargs["M_N"]  # Account for the minibatch samples from the dataset
-        recons_loss = F.mse_loss(recons, input)
-
-        kld_loss = torch.mean(
-            -0.5 * torch.sum(1 + log_var - mu**2 - log_var.exp(), dim=1), dim=0
+        return ForwardReturn(
+            output=self.decode(z), input=input, encoded=latents, latents=z
         )
 
-        loss = recons_loss + kld_weight * kld_loss
-        return {"loss": loss, "Reconstruction_Loss": recons_loss, "KLD": -kld_loss}
-
-    def sample(self, num_samples: int, current_device: int, **kwargs) -> torch.tensor:
+    def sample(self, num_samples: int, labels: torch.tensor) -> torch.tensor:
         """
         Samples from the latent space and return the corresponding
         image space map.
@@ -165,20 +52,18 @@ class ConditionalVAE(BaseVAE):
         :param current_device: (Int) Device to run the model
         :return: (torch.tensor)
         """
-        y = kwargs["labels"].float()
+        y = labels.float()
         z = torch.randn(num_samples, self.latent_dim)
-
-        z = z.to(current_device)
 
         z = torch.cat([z, y], dim=1)
         samples = self.decode(z)
         return samples
 
-    def generate(self, x: torch.tensor, **kwargs) -> torch.tensor:
+    def generate(self, x: torch.tensor, labels: torch.tensor) -> torch.tensor:
         """
         Given an input image x, returns the reconstructed image
         :param x: (torch.tensor) [B x C x H x W]
         :return: (torch.tensor) [B x C x H x W]
         """
 
-        return self.forward(x, **kwargs)[0]
+        return self.forward(x, labels)["output"]
