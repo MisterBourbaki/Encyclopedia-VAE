ba44e4bccd81cc4f8457f5b1ac3515b4e16dc9bb
BaptisteMorisse <bmorisse.pro@proton.me>
2024-05-16 11:31:33 +0200

    Forget something...

--- encyclopedia_vae/cli.py
+++ encyclopedia_vae/cli.py
@@ -1,11 +1,11 @@
 from lightning.pytorch.cli import LightningCLI
 
-from encyclopedia_vae.data.lit_celeba import LitCelebA
+from encyclopedia_vae.data.lit_oxfordiitpet import LitOxfordIIITPet
 from encyclopedia_vae.lit_models.lit_vae import LitVAE
 
 
 def cli_main():
-    cli = LightningCLI(LitVAE, LitCelebA)  # noqa F841
+    cli = LightningCLI(LitVAE, LitOxfordIIITPet)  # noqa F841
 
 
 if __name__ == "__main__":
